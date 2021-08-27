79ee0b494deb515e737575f7dda2e0f860938b3d
M1chelle99 <MichelleBecker2003@gmail.com>
2021-08-27 11:34:12 +0200

    Init

---
+++ dirr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dirr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
